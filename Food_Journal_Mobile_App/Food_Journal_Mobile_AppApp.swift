10d7d8b4185e75e7233fb4b15088c6a42cb050ed
Billie <150131870+FalseMe9@users.noreply.github.com>
2025-04-23 07:30:50 +0700

    Initial Commit

---
+++ Food_Journal_Mobile_App/Food_Journal_Mobile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Journal_Mobile_AppApp.swift
+//  Food_Journal_Mobile_App
+//
+//  Created by Billie H on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Food_Journal_Mobile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
